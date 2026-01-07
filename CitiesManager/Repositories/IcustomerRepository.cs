798f6a75ea2799d1d6b0ffdc2e091750cfdf7c37
nayazi ifroz khan <108205007+ifrozkhan@users.noreply.github.com>
2026-01-07 20:17:39 +0530

    created a Asp.net core web api project

---
+++ CitiesManager/Repositories/IcustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using CitiesManager.WebAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CitiesManager.WebAPI.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Employee_Details>> GetAllAsync();
+
+        Task<Employee_Details?> GetByIdAsync(int id);
+
+        Task<string> CreateAsync(Employee_Details customer);
+
+        Task<string> UpdateAsync(Employee_Details customer, int id);
+
+        Task<string> DeleteAsync(int id);
+    }
+}
